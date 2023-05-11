5c8c3874b156f7e3ff15e37b66c9304ef0cdaf75
Crawalrutvik <rutvikraval.crawlapps@gmail.com>
2023-05-11 10:32:50 +0530

    holireads

---
+++ assets/js/custom.js
@@ -0,0 +1,273 @@
+
+$('.slider6').slick({
+    arrows: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 9,
+    slidesToScroll: 1,
+    prevArrow: '<span class="priv_arrow"> <img src="assets/imges/back arrow.png" ></span>',
+    nextArrow: '<span class="next_arrow"> <img src="assets/imges/back arrow1.png" ></span>',
+    responsive: [
+        {
+            breakpoint: 2000,
+            settings: {
+                slidesToShow: 8,
+                slidesToScroll: 1,
+                infinite: true,
+                
+            }
+        },
+        {
+            breakpoint: 1800,
+            settings: {
+                slidesToShow: 7,
+                slidesToScroll: 1,
+                infinite: true,
+                
+            }
+        },
+        {
+            breakpoint: 1600,
+            settings: {
+                slidesToShow: 6,
+                slidesToScroll: 1,
+                infinite: true,
+                
+            }
+        },
+        {
+            breakpoint: 1440,
+            settings: {
+                slidesToShow: 5,
+                slidesToScroll: 1,
+                infinite: true,
+                
+            }
+        },
+        {
+            breakpoint: 1300,
+            settings: {
+                slidesToShow: 4,
+                slidesToScroll: 1,
+                infinite: true,
+                
+            }
+        },
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: true,
+                
+            }
+        },
+        {
+            breakpoint: 991,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                infinite: true,
+                
+            }
+        },
+        {
+            breakpoint: 767,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 575,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
+
+
+
+
+$('.slider4').slick({
+    arrows: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow: '<span class="priv_arrow"> <img src="assets/imges/back arrow.png" ></span>',
+    nextArrow: '<span class="next_arrow"> <img src="assets/imges/back arrow1.png" ></span>',
+    responsive: [
+        {
+            breakpoint: 1800,
+            settings: {
+                slidesToShow: 4,
+                slidesToScroll: 1,
+                infinite: true,
+                
+            }
+        },
+        // {
+        //     breakpoint: 1500,
+        //     settings: {
+        //         slidesToShow: 4,
+        //         slidesToScroll: 1,
+        //         infinite: true,
+                
+        //     }
+        // },
+        {
+            breakpoint: 1445,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: true,
+                
+            }
+        },
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 3,
+                infinite: true,
+                
+            }
+        },
+        // {
+        //     breakpoint: 991,
+        //     settings: {
+        //         slidesToShow: 1,
+        //         slidesToScroll: 1,
+        //         infinite: true,
+        //     }
+        // },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
+
+
+
+
+$('.slider0').slick({
+    arrows: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    prevArrow: '<span class="priv_arrow0"> <img src="assets/imges/back arrow.png" ></span>',
+    nextArrow: '<span class="next_arrow0"> <img src="assets/imges/back arrow1.png" ></span>',
+    responsive: [
+        {
+            breakpoint: 1500,
+            settings: {
+                slidesToShow: 6,
+                slidesToScroll: 1,
+                infinite: true,
+            }
+        },
+        {
+            breakpoint: 1300,
+            settings: {
+                slidesToShow: 5,
+                slidesToScroll: 1,
+                infinite: true,
+            }
+        },
+        {
+            breakpoint: 1200,
+            settings: {
+                slidesToShow: 4 ,
+                slidesToScroll: 1,
+                infinite: true,
+            }
+        },
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: true,
+            }
+        },
+        {
+            breakpoint: 991,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                infinite: true,
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
+
+
+$('.banner-slider').slick({
+    arrows: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: '<span class="banner-pri"> <img src="assets/imges/banner-pri.png" ></span>',
+    nextArrow: '<span class="banne-next"> <img src="assets/imges/banne-next.png" ></span>',
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
